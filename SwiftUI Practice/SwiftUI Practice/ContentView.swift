ffa27fa27ab406ec5f398d499fa0e26f19e4f083
key5771 <ksj57715@hanmail.net>
2020-10-13 14:23:00 +0900

    create project

---
+++ SwiftUI Practice/SwiftUI Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Practice
+//
+//  Created by 김기현 on 2020/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .font(.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
