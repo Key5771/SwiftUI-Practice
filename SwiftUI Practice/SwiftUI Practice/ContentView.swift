929b5a26e60ba0eaab2003c6bb0b05ba05c3f49e
key5771 <ksj57715@hanmail.net>
2020-10-13 14:50:16 +0900

    font & foregroundColor change Generate previews Group

--- SwiftUI Practice/SwiftUI Practice/ContentView.swift
+++ SwiftUI Practice/SwiftUI Practice/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .padding()
             .font(.largeTitle)
+            .foregroundColor(.red)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+                .previewDisplayName("iPhone 11 Pro Max")
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+                .previewDisplayName("iPhone 8")
+                .environment(\.colorScheme, .dark)
+        }
     }
 }
