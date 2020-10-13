b46b35b405c600bb6d3aaf627ec80643a8067208
key5771 <ksj57715@hanmail.net>
2020-10-13 15:12:46 +0900

    Text 속성 변경

--- SwiftUI Practice/SwiftUI Practice/ContentView.swift
+++ SwiftUI Practice/SwiftUI Practice/ContentView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, SwiftUI!")
+            .fontWeight(.bold)
             .padding()
             .font(.largeTitle)
             .foregroundColor(.red)
+            .colorInvert()
     }
 }
 
